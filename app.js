3323295243e0ab33d4c40a63700f0f0e5ad4b2cb
juvisingh <141285456+juvisingh@users.noreply.github.com>
2024-03-11 17:49:19 -0700

    updated the main page

---
+++ app.js
@@ -0,0 +1,53 @@
+var resume = document.getElementById('resume')
+resume.addEventListener('mouseover', () => {
+    resume.style.color = 'red';
+    resume.style.cursor ='pointer';
+});
+resume.addEventListener('mouseout', () => {
+    resume.style.color = 'white';
+});
+var gallery = document.getElementById('gallery')
+gallery.addEventListener('mouseover', () => {
+    gallery.style.color = 'red';
+    gallery.style.cursor ='pointer';
+});
+gallery.addEventListener('mouseout', () => {
+    gallery.style.color = 'white';
+});
+var about = document.getElementById('about')
+about.addEventListener('mouseover', () => {
+    about.style.color = 'red';
+    about.style.cursor ='pointer';
+});
+about.addEventListener('mouseout', () => {
+    about.style.color = 'white';
+});
+var contact = document.getElementById('contact')
+contact.addEventListener('mouseover', () => {
+    contact.style.color = 'red';
+    contact.style.cursor ='pointer';
+});
+contact.addEventListener('mouseout', () => {
+    contact.style.color = 'white';
+});
+
+var mainBodyContent = document.getElementById('main-content')
+//sets the body display to grid
+mainBodyContent.style.display = "grid"
+mainBodyContent.style.padding = "10px"
+//sets the rows and columns for the grid, where the image and the text will go
+mainBodyContent.style.gridTemplateColumns = "[f] 50% [s] 50% [end]"
+mainBodyContent.style.gridTemplateRows = "[first] auto [end]"
+
+
+document.getElementById("juviImage").style.width = "-webkit-fill-available"
+document.getElementById("juviImage").style.padding = "20px"
+document.getElementById("juviImage").style.borderRadius = "50%"
+//makes the content be aligned in the middle for the text. The rest also adds css to the h1 tag and text
+document.getElementById("mainText").style.textAlign = "center"
+document.getElementById("mainText").style.padding = "20px"
+document.getElementById("mainText").style.color = "black"
+document.getElementById("mainText").style.fontSize = "1.71vw"
+document.getElementById("mainText").style.width = "-webkit-fill-available"
+document.getElementById("mainText").style.display = "flex"
+document.getElementById("mainText").style.alignItems = "center"
