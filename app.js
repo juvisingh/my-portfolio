1393c5ed4802b6f78e1473fd100613f6f5f9c612
juvisingh <141285456+juvisingh@users.noreply.github.com>
2024-04-07 15:00:03 -0700

    updates

--- app.js
+++ app.js
@@ -31,23 +31,31 @@ contact.addEventListener('mouseout', () => {
     contact.style.color = 'white';
 });
 
-var mainBodyContent = document.getElementById('main-content')
-//sets the body display to grid
-mainBodyContent.style.display = "grid"
-mainBodyContent.style.padding = "10px"
-//sets the rows and columns for the grid, where the image and the text will go
-mainBodyContent.style.gridTemplateColumns = "[f] 50% [s] 50% [end]"
-mainBodyContent.style.gridTemplateRows = "[first] auto [end]"
+
+$document.ready(function(){
+    $('#name').hide(1000)
+});
 
 
-document.getElementById("juviImage").style.width = "-webkit-fill-available"
-document.getElementById("juviImage").style.padding = "20px"
-document.getElementById("juviImage").style.borderRadius = "20%"
-//makes the content be aligned in the middle for the text. The rest also adds css to the h1 tag and text
-document.getElementById("mainText").style.textAlign = "center"
-document.getElementById("mainText").style.padding = "20px"
-document.getElementById("mainText").style.color = "black"
-document.getElementById("mainText").style.fontSize = "1.71vw"
-document.getElementById("mainText").style.width = "-webkit-fill-available"
-document.getElementById("mainText").style.display = "flex"
-document.getElementById("mainText").style.alignItems = "center"
+
+
+// var mainBodyContent = document.getElementById('main-content')
+// //sets the body display to grid
+// mainBodyContent.style.display = "grid"
+// mainBodyContent.style.padding = "10px"
+// //sets the rows and columns for the grid, where the image and the text will go
+// mainBodyContent.style.gridTemplateColumns = "[f] 50% [s] 50% [end]"
+// mainBodyContent.style.gridTemplateRows = "[first] auto [end]"
+
+
+// document.getElementById("juviImage").style.width = "-webkit-fill-available"
+// document.getElementById("juviImage").style.padding = "20px"
+// document.getElementById("juviImage").style.borderRadius = "20%"
+// //makes the content be aligned in the middle for the text. The rest also adds css to the h1 tag and text
+// document.getElementById("mainText").style.textAlign = "center"
+// document.getElementById("mainText").style.padding = "20px"
+// document.getElementById("mainText").style.color = "black"
+// document.getElementById("mainText").style.fontSize = "1.71vw"
+// document.getElementById("mainText").style.width = "-webkit-fill-available"
+// document.getElementById("mainText").style.display = "flex"
+// document.getElementById("mainText").style.alignItems = "center"
