e4756ba3280b09343f6d1330c47bc46194a86a1f
juvisingh <141285456+juvisingh@users.noreply.github.com>
2024-04-17 18:11:34 -0700

    finished gallery

--- app1.js
+++ app1.js
@@ -17,3 +17,36 @@ next.addEventListener("click", () => {
 function rotate() {
   imageContainer.style.transform = `perspective(1000px) rotateY(${x}deg)`;
 }
+
+var resume = document.getElementById('resume')
+resume.addEventListener('mouseover', () => {
+    resume.style.color = 'red';
+    resume.style.cursor ='pointer';
+});
+resume.addEventListener('mouseout', () => {
+    resume.style.color = 'white';
+});
+var gallery = document.getElementById('gallery')
+gallery.addEventListener('mouseover', () => {
+    gallery.style.color = 'red';
+    gallery.style.cursor ='pointer';
+});
+gallery.addEventListener('mouseout', () => {
+    gallery.style.color = 'white';
+});
+var about = document.getElementById('about')
+about.addEventListener('mouseover', () => {
+    about.style.color = 'red';
+    about.style.cursor ='pointer';
+});
+about.addEventListener('mouseout', () => {
+    about.style.color = 'white';
+});
+var contact = document.getElementById('contact')
+contact.addEventListener('mouseover', () => {
+    contact.style.color = 'red';
+    contact.style.cursor ='pointer';
+});
+contact.addEventListener('mouseout', () => {
+    contact.style.color = 'white';
+});
